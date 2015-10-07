fb8f953b4b98703e6f95e7de1d4badac60eb9398
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-06 17:35:44 -0700

    Styling of elements.

--- wid-card-level-con.js
+++ wid-card-level-con.js
@@ -16,7 +16,7 @@
       level: {
         type: Number,
         value: 0,
-        observer: '_conLevelChanged'
+        observer: '_onLevelChanged'
       },
 
       /**
@@ -48,9 +48,32 @@
 
     },
 
-    _conLevelChanged: function _conLevelChanged(newVal) {
-      this.$.descHeaderContainer.setAttribute('class', 'flex layout horizontal center-center style-scope wid-card-level-con header-level-' + newVal);
+    _onLevelChanged: function _onLevelChanged(newVal) {
+      this.$.descHeaderContainer.setAttribute('class', 'flex-2 layout horizontal center-center style-scope wid-card-level-con header-level-' + newVal);
       this.$.descStatusContainer.setAttribute('class', 'row-icon flex-3 layout vertical center-center style-scope wid-card-level-con status-level-' + newVal);
+      this.$.cloudIcon.src = 'bower_components/wid-card-level-con/' + this._getIconLevel() + '.svg';
+    },
+
+    _getIconLevel: function _getIconLevel() {
+      var levelIcon = '';
+      switch (this.level) {
+        case 0:
+          levelIcon = 'sunny';
+          break;
+        case 1:
+          levelIcon = 'cloudy';
+          break;
+        case 2:
+          levelIcon = 'rainy';
+          break;
+        case 3:
+          levelIcon = 'stormy';
+          break;
+        case 4:
+          levelIcon = 'thunderous';
+          break;
+      }
+      return levelIcon;
     }
 
   });
